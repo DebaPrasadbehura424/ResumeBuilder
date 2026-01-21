6639a0a944631fd9fdff9700c8638bf0746d0b3d
DebaPrasadbehura424 <debaprasadbehura89@gmail.com>
2026-01-21 19:53:55 +0530

    rec update 2

--- backend_resume/database/db.js
+++ backend_resume/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost:27017/ResumeMaker");
 mongoose.connect(
-  "mongodb+srv://debaprasadbehura89:s5dY5LSzdhymR4AB@cluster0.9chhe.mongodb.net/Resume_Maker?retryWrites=true&w=majority&appName=Cluster0;"
+  "mongodb+srv://debaprasadbehura89:s5dY5LSzdhymR4AB@cluster0.9chhe.mongodb.net/Resume_Maker?retryWrites=true&w=majority&appName=Cluster0;",
 );
 
 const db = mongoose.connection;
