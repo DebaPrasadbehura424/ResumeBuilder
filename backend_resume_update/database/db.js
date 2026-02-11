5bff7abf647c41c61c0b5ccc972f7bf4a5c94840
DebaPrasadbehura424 <debaprasadbehura89@gmail.com>
2026-02-11 23:28:20 +0530

    db 2

--- backend_resume_update/database/db.js
+++ backend_resume_update/database/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const URI =
-  "mongodb+srv://debaprasadbehura89:iunnotfound@cluster0.9chhe.mongodb.net/ResumeMaker?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://debaprasadbehura89:password123456@cluster0.9chhe.mongodb.net/ResumeMaker?retryWrites=true&w=majority&appName=Cluster0";
+
 // const URI = "mongodb://localhost:27017/ResumeMaker";
 mongoose
   .connect(URI)
